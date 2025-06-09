8c88fd7484aded44e332c2143fd9d644a3369577
Mozart Maia <mozartmaia89@gmail.com>
2025-06-09 03:28:54 -0300

    feat: pinos de ci SN74HC166. parte da logica de leitura serial.

--- tarefa8.c
+++ tarefa8.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <math.h>
 
 #define I2C_SDA_PIN 14 
 #define I2C_SCL_PIN 15
@@ -14,6 +15,10 @@
 #define OLED_HEIGHT 32
 
 #define BUTTON_B 6
+
+#define LOAD_PIN 28
+#define CLK_PIN 17
+#define SERIAL_IN_PIN 16
 
 
 void show_message_oled(char* message[], int lines) {
@@ -78,7 +83,32 @@ void oled_setup() {
 void gpio_setup() {
   gpio_init(BUTTON_B);
   gpio_set_dir(BUTTON_B, GPIO_IN);
-  gpio_pull_up(BUTTON_B);
+  gpio_pull_down(BUTTON_B);
+
+  /* De acordo com o datasheet do SN74HC166 :
+        The SNx4HC166 is a parallel-load 8-bit shift register with asynchronous clear ( CLR). This parallel-in or serial-in,
+    serial-out shift register features gated clock (CLK, CLK INH) inputs and an overriding clear (CLR) input. The
+    parallel-in or serial-in modes are established by the mode select (SH/LD) input. When high, SH/LD enables the
+    serial (SER) data input and couples the eight flip-flops for serial shifting with each clock (CLK) pulse. When low,
+    the parallel (A through H) data inputs are enabled, and synchronous loading occurs on the next clock pulse.
+    During parallel loading, serial data flow is inhibited. Clocking is accomplished on the rising edge of CLK or CLK
+    INH, permitting one input to be used as a clock-enable or clock-inhibit function. Holding either CLK or CLK INH
+    high inhibits clocking; holding either low enables the other clock input. CLK INH should be changed to the high 
+  */ 
+  // pino para alternar entre função de shift output (1) e load input (0)
+  gpio_init(LOAD_PIN);
+  gpio_set_dir(LOAD_PIN, GPIO_OUT);
+  gpio_pull_down(LOAD_PIN);
+
+  // pino do clock para alternar no ci SN74HC166
+  gpio_init(CLK_PIN);
+  gpio_set_dir(CLK_PIN, GPIO_OUT);
+  gpio_pull_down(CLK_PIN);
+
+  // pino que vai receber o sinal serial a cada pulso de clock no SN74HC166
+  gpio_init(SERIAL_IN_PIN);
+  gpio_set_dir(SERIAL_IN_PIN, GPIO_IN);
+  gpio_pull_down(SERIAL_IN_PIN);
 }
 
 int main()
@@ -86,19 +116,35 @@ int main()
     stdio_init_all();
     oled_setup();
     gpio_setup();
+
+    int a,b,c,d,e,f,g,h;
     
     int button_b_state = 0;
-    // button_b_state = gpio_get(BUTTON_B);
-    gpio_put(BUTTON_B, 1);
+    
+    // gpio_put(BUTTON_B, 1);
 
     char *text[] = {
-        "teste",
+        "VAL",
+        "10"
         };
     // printf("texto:  \n");
     show_message_oled(text,1);
     
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        button_b_state = gpio_get(BUTTON_B);
+        if (button_b_state) {
+            double decimal_number = 0;
+            printf("botao pressionado!\n");
+            gpio_put(LOAD_PIN, true);
+            for (int i=0; i < 9; i++){
+                gpio_put(CLK_PIN, 1);
+                a = gpio_get(SERIAL_IN_PIN);
+                decimal_number += a * (pow(2, (double)i));
+                gpio_put(CLK_PIN, 0);
+            }
+            gpio_put(LOAD_PIN, true);
+            printf("numero lido: %d" , decimal_number);            
+        }       
+        sleep_ms(100);
     }
 }
