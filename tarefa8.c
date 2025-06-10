576e2c2fe058a2680d6cae505bf8db317c0d492f
Mozart Maia <mozartmaia89@gmail.com>
2025-06-10 20:22:35 -0300

    fix: comentando funcao nao utilizada

--- tarefa8.c
+++ tarefa8.c
@@ -14,31 +14,31 @@
 #include "hardware/structs/pll.h"
 #include "hardware/structs/clocks.h"
 
-void measure_freqs(void) {
-    uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
-    uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
-    uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
-    uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
-    uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
-    uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
-    uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
-#ifdef CLOCKS_FC0_SRC_VALUE_CLK_RTC
-    uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
-#endif
+// void measure_freqs(void) {
+//     uint f_pll_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_SYS_CLKSRC_PRIMARY);
+//     uint f_pll_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_PLL_USB_CLKSRC_PRIMARY);
+//     uint f_rosc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_ROSC_CLKSRC);
+//     uint f_clk_sys = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_SYS);
+//     uint f_clk_peri = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_PERI);
+//     uint f_clk_usb = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_USB);
+//     uint f_clk_adc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_ADC);
+// #ifdef CLOCKS_FC0_SRC_VALUE_CLK_RTC
+//     uint f_clk_rtc = frequency_count_khz(CLOCKS_FC0_SRC_VALUE_CLK_RTC);
+// #endif
 
-    printf("pll_sys  = %dkHz\n", f_pll_sys);
-    printf("pll_usb  = %dkHz\n", f_pll_usb);
-    printf("rosc     = %dkHz\n", f_rosc);
-    printf("clk_sys  = %dkHz\n", f_clk_sys);
-    printf("clk_peri = %dkHz\n", f_clk_peri);
-    printf("clk_usb  = %dkHz\n", f_clk_usb);
-    printf("clk_adc  = %dkHz\n", f_clk_adc);
-#ifdef CLOCKS_FC0_SRC_VALUE_CLK_RTC
-    printf("clk_rtc  = %dkHz\n", f_clk_rtc);
-#endif
+//     printf("pll_sys  = %dkHz\n", f_pll_sys);
+//     printf("pll_usb  = %dkHz\n", f_pll_usb);
+//     printf("rosc     = %dkHz\n", f_rosc);
+//     printf("clk_sys  = %dkHz\n", f_clk_sys);
+//     printf("clk_peri = %dkHz\n", f_clk_peri);
+//     printf("clk_usb  = %dkHz\n", f_clk_usb);
+//     printf("clk_adc  = %dkHz\n", f_clk_adc);
+// #ifdef CLOCKS_FC0_SRC_VALUE_CLK_RTC
+//     printf("clk_rtc  = %dkHz\n", f_clk_rtc);
+// #endif
 
-    // Can't measure clk_ref / xosc as it is the ref
-}
+//     // Can't measure clk_ref / xosc as it is the ref
+// }
 
 #define I2C_SDA_PIN 14 
 #define I2C_SCL_PIN 15
@@ -124,7 +124,9 @@ void gpio_setup() {
     the parallel (A through H) data inputs are enabled, and synchronous loading occurs on the next clock pulse.
     During parallel loading, serial data flow is inhibited. Clocking is accomplished on the rising edge of CLK or CLK
     INH, permitting one input to be used as a clock-enable or clock-inhibit function. Holding either CLK or CLK INH
-    high inhibits clocking; holding either low enables the other clock input. CLK INH should be changed to the high 
+    high inhibits clocking; holding either low enables the other clock input. CLK INH should be changed to the high
+    level only when CLK is high.
+    CLR overrides all other inputs, including CLK, and resets all flip-flops to zero. 
   */ 
   // pino para alternar entre função de shift output (1) e load input (0)
   gpio_init(LOAD_PIN);
